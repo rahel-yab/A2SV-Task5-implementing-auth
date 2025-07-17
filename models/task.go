b061b72bf4f6dc3506b56fffeda333fa681366e0
rahel yabebal <rahelyabebal@gmail.com>
2025-07-17 15:33:25 +0300

    refactor: map Task ID to MongoDB _id field

--- models/task.go
+++ models/task.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Task struct {
- ID          string    `json:"id"`
- Title       string    `json:"title"`
- Description string    `json:"description"`
- DueDate     time.Time `json:"due_date"`
- Status      string    `json:"status"`
+	ID          string    `json:"id" bson:"_id"`
+	Title       string    `json:"title" bson:"title"`
+	Description string    `json:"description" bson:"description"`
+	DueDate     time.Time `json:"due_date" bson:"due_date"`
+	Status      string    `json:"status" bson:"status"`
 }
